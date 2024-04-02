f7218e7f541fe7a2af9c683355fbccd4bc5db6f6
joaquinbian <joacobian21@gmail.com>
2024-04-02 17:29:13 -0300

    git ignore

---
+++ ejercicio-15/is_leap.cpp
@@ -0,0 +1,15 @@
+//importamos libreria para hacer print e ingresar info 
+#include <iostream>
+
+//usamos el namespace
+using namespace std;
+
+int main(){
+ 
+    int year;
+
+    cout << "Ingresa un anio y te diremos si es bisiesto o no " << endl;
+
+    
+    return 0;
+}
